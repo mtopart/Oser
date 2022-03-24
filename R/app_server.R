7858d2bf2d028868f3fa5ceacaf2ca456059622b
mtopart <m.topart@agro-transfert-rt.org>
2022-03-24 15:41:01 +0100

    reintegration modifs modules oser test

--- R/app_server.R
+++ R/app_server.R
@@ -8,17 +8,37 @@ app_server <- function( input, output, session ) {
 
   
   
-  r <- reactiveValues(
-    ch = NULL 
-  )
+  r <- reactiveValues()
   
   # List the first level callModules here
+  
+  
+  # Mod distrib
+  
+  mod_box_distrib_server("box_distrib_ui_1",
+                         graph_title = "Répartition de la production",
+                         type = "production",
+                         r = r)
+  
+  mod_box_distrib_server("box_distrib_ui_2",
+                         graph_title = "Répartition du prix",
+                         type = "prix",
+                         r = r)
+  
+  mod_box_distrib_server("box_distrib_ui_3",
+                         graph_title = "Répartition des charges",
+                         type = "charges",
+                         r = r)
+  
+  mod_aide_distrib_server("aide_distrib_ui_1")
+  
+  mod_graph_final_server("graph_final_ui_1",
+                         r = r)
  
   mod_choix_unit_server("choix_unit_ui_1", r = r)
-  mod_Oser_server("Oser_ui_1", r = r)
+ 
   mod_tutoriel_server("tutoriel_ui_1")
   mod_apropos_server("apropos_ui_1")
-  mod_charges_server("charges_ui_1", r = r)
   
   
  
